28c88c7ba97b84728a86f60a20e8010ed99eeffc
kc <kc.dark@protonmail.com>
2022-02-26 19:35:38 +0530

    Lesson 7

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,13 @@
 // Importing modules
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const dotenv = require('dotenv').config();
 
 passport.use(
     new GoogleStrategy({
         // options for the google startegy
-        clientID: '',
-        clientSecret: '' 
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
     }), () => {
         // passport callback funtion
     }
